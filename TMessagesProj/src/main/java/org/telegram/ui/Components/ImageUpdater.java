9f3813f09b14a116cdf342885c974f2a4e921f3a
dkaraush <dkaraush@gmail.com>
2024-10-18 00:04:16 +0400

    update to 11.2.3 (5335)
    (cherry picked from commit 9b78d437de6ec1bbf067e2f17221b38db49f064d)

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ImageUpdater.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ImageUpdater.java
@@ -26,7 +26,6 @@ import android.util.Pair;
 import android.view.View;
 
 import androidx.core.content.FileProvider;
-import androidx.exifinterface.media.ExifInterface;
 
 import org.cloudveil.messenger.CloudVeilSecuritySettings;
 import org.telegram.messenger.AndroidUtilities;
@@ -862,7 +861,7 @@ public class ImageUpdater implements NotificationCenter.NotificationCenterDelega
                     NotificationCenter.getInstance(currentAccount).addObserver(ImageUpdater.this, NotificationCenter.filePreparingStarted);
                     NotificationCenter.getInstance(currentAccount).addObserver(ImageUpdater.this, NotificationCenter.filePreparingFailed);
                     NotificationCenter.getInstance(currentAccount).addObserver(ImageUpdater.this, NotificationCenter.fileNewChunkAvailable);
-                    MediaController.getInstance().scheduleVideoConvert(avatarObject, true, true);
+                    MediaController.getInstance().scheduleVideoConvert(avatarObject, true, true, false);
                     uploadingImage = null;
                     if (delegate != null) {
                         delegate.didStartUpload(true);
--- TMessagesProj/src/main/java/org/telegram/ui/ContactsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ContactsActivity.java
@@ -337,7 +337,7 @@ public class ContactsActivity extends BaseFragment implements NotificationCenter
                     floatingButtonContainer.setTranslationY(AndroidUtilities.dp(100));
                     hideFloatingButton(false);
                 }
-                if (sortItem != null) {
+                if (sortItem != null && !listViewAdapter.isEmpty) {
                     sortItem.setVisibility(View.VISIBLE);
                 }
             }
@@ -408,6 +408,9 @@ public class ContactsActivity extends BaseFragment implements NotificationCenter
                         //emptyView.setVisibility(count == 0 ? View.VISIBLE : View.GONE);
                         listView.setFastScrollVisible(count != 0);
                     }
+                }
+                if (sortItem != null) {
+                    sortItem.setVisibility(!isEmpty && !item.isSearchFieldVisible() ? View.VISIBLE : View.GONE);
                 }
             }
         };
