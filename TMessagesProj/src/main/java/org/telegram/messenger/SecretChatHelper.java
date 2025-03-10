f13c80084dc91f8f5b0de649e9263e537e02c081
dkaraush <dkaraush@gmail.com>
2025-03-10 17:15:31 +0400

    update to 11.8.0 (5771)

--- TMessagesProj/src/main/java/org/telegram/messenger/SecretChatHelper.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/SecretChatHelper.java
@@ -18,7 +18,6 @@ import android.util.SparseIntArray;
 import org.cloudveil.messenger.CloudVeilSecuritySettings;
 import org.telegram.SQLite.SQLiteCursor;
 import org.telegram.messenger.support.LongSparseIntArray;
-import org.telegram.tgnet.AbstractSerializedData;
 import org.telegram.tgnet.ConnectionsManager;
 import org.telegram.tgnet.InputSerializedData;
 import org.telegram.tgnet.NativeByteBuffer;
@@ -1436,7 +1435,7 @@ public class SecretChatHelper extends BaseController {
                         TLRPC.Message message = messages.get(a);
                         MessageObject messageObject = new MessageObject(currentAccount, message, false, true);
                         messageObject.resendAsIs = true;
-                        getSendMessagesHelper().retrySendMessage(messageObject, true);
+                        getSendMessagesHelper().retrySendMessage(messageObject, true, 0);
                     }
                 });
 
