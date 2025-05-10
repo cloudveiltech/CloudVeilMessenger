480051503fb0a4fb407734861598d71ec00cde90
sirsoweird <26212881+sirsoweird@users.noreply.github.com>
2025-05-10 08:55:26 -0600

    Centre CV logo intro, don't change Telegram Premium etc. to CV Premium. Should go to production

--- TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
@@ -1373,7 +1373,10 @@ public class LocaleController {
         }
         //CloudVeil start
         String appName = ApplicationLoader.applicationContext.getString(R.string.AppName);
-        return value.replace("Telegram", appName);
+        if (value.contains("Telegram Premium") || value.contains("Telegram Business") || value.contains("Telegram Stars")) {
+            return value;  // leave as 'Telegram', less confusion for users
+        }
+        return value.replace("Telegram", appName); // otherwise 'CloudVeil Messenger' branding
         //CloudVeil end
     }
 
