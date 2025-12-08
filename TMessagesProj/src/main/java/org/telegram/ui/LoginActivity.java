31622697a0340cc615fa824e9823df93c2f2eb43
Arseny271 <arsendmitr@gmail.com>
2025-12-08 21:02:56 +0400

    update to 12.2.9 (6337)

--- TMessagesProj/src/main/java/org/telegram/ui/LoginActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LoginActivity.java
@@ -3484,7 +3484,9 @@ public class LoginActivity extends BaseFragment implements NotificationCenter.No
                         }
                     }
                 }
-                requestPasskey(false);
+                if (activityMode == MODE_LOGIN) {
+                    requestPasskey(false);
+                }
             }, SHOW_DELAY);
         }
 
@@ -3501,6 +3503,7 @@ public class LoginActivity extends BaseFragment implements NotificationCenter.No
         private boolean requestingPasskey = false;
         private Runnable cancelRequestingPasskey;
         private void requestPasskey(boolean clickedButton) {
+            if (activityMode != MODE_LOGIN) return;
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P || !BuildVars.SUPPORTS_PASSKEYS) return;
             if (requestingPasskey || !clickedButton && requestedPasskey) return;
 
@@ -3510,7 +3513,7 @@ public class LoginActivity extends BaseFragment implements NotificationCenter.No
                 cancelRequestingPasskey = null;
                 requestingPasskey = false;
                 if (err != null && ("EMPTY".equals(err) || "CANCELLED".equals(err))) {
-                    if (subtitleView != null) {
+                    if (subtitleView != null && "CANCELLED".equals(err)) {
                         subtitleView.setText(AndroidUtilities.replaceArrows(AndroidUtilities.replaceSingleTag(getString(R.string.StartTextPasskey), () -> {
                             requestPasskey(true);
                         }), true));
