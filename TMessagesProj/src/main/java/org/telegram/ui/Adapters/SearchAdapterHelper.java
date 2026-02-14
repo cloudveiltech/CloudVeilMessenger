b542c36ab25aa00b110d95fee6b1eee9d429e426
Arseny271 <arsendmitr@gmail.com>
2026-02-14 16:49:34 +0400

    update to 12.4.0 (6506)
    (cherry picked from commit fa4e8aa0ddbd2416c382d36eaab6ec1c08e9fa00)

--- TMessagesProj/src/main/java/org/telegram/ui/Adapters/SearchAdapterHelper.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Adapters/SearchAdapterHelper.java
@@ -311,10 +311,6 @@ public class SearchAdapterHelper {
             hasChanged = false;
         }
 
-        if (hasChanged) {
-            delegate.onDataSetChanged(searchId);
-        }
-
         final AtomicInteger gotResponses = new AtomicInteger(0);
         final ArrayList<Pair<TLObject, TLRPC.TL_error>> responses = new ArrayList<>();
         for (int i = 0; i < requests.size(); ++i) {
@@ -350,6 +346,10 @@ public class SearchAdapterHelper {
                 }
             })));
             pendingRequestIds.add(reqId.get());
+        }
+
+        if (hasChanged) {
+            delegate.onDataSetChanged(searchId);
         }
     }
 
